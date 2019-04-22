efe781ee15838b43a5791ea377f244a9cce0ebeb
Эмиль Эмиль <imys@yandex.ru>
2019-04-22 20:44:48 +0300

    Это нащ первый коммит

---
+++ main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// var arr = ('fuck');
+
+// // console.log(arr);
+
+// // alert('fuck');
+// // let answer = confirm("Are you here?");
+
+// // console.log(answer);
+// let answer = prompt("Есть ли вам 18", "ДА");
+
+// console.log(typeof(arr));
+
+
+// console.log("arr" + " - object");
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(--decr);
+
+let isChecket = false,
+    isClose = true;
+
+console.log(isChecket || !isClose);
