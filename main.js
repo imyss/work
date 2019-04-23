fb95ddd8d7f29fa31a8bcf6de935d49da6970645
Эмиль Эмиль <imys@yandex.ru>
2019-04-23 19:50:04 +0300

    Первые шаги

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
-'use strict';
+"use strict";
+
+// let greeting = "Добро пожаловать на мою домашнюю страницу," + " " + name;
+
+// let msg = "Hello, " + "world."; // Получается строка "Hello, world"
+// console.log(greeting, m);
+last_char = s.charAt(s.length – 1);
+console.log(last_char);
+
+// var money;
+// var time;
+// money = prompt('Ваш бюджет в месяц?', '');
+// time = prompt('Введите дату в формате YYYY-MM-DD', '');
+// let appData = {
+//     budjet: money,
+//     timeData: time,
+//     expenses: {
+//         'Введите обязательную статью расходов в этом месяце' : 'Во сколько обойдется?'
+//         },
+//     optionalExpenses: {},
+//     income: [],
+//     saving: false
+// };
+
+// appData.expenses.alert(money);
+
+
 
 // var arr = ('fuck');
 
@@ -14,15 +40,15 @@
 
 
 // console.log("arr" + " - object");
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
 
 
-console.log(incr++);
-console.log(--decr);
+// console.log(incr++);
+// console.log(--decr);
 
-let isChecket = false,
-    isClose = true;
+// let isChecket = false,
+//     isClose = true;
 
-console.log(isChecket || !isClose);
+// console.log(isChecket || !isClose);
